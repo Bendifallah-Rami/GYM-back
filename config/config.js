4ccf4d9bb352eaa1012a2a530f51b6e6cf888ae1
Bendifallah-Rami <nr_bendifallah@esi.dz>
2025-12-01 19:35:27 +0100

    Fix Sequelize config for production environment on Render

--- config/config.js
+++ config/config.js
@@ -26,11 +26,7 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
@@ -38,6 +34,12 @@ module.exports = {
         require: true,
         rejectUnauthorized: false
       }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
   }
 };
